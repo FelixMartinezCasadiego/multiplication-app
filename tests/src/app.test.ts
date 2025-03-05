b1a36c35b820e66c9fc5547eb693122b783de437
Felix Martinez Casadiego <felix.martinez@simonelectric.com>
2025-03-05 22:21:04 +0100

    test: add unit test about server-app and app.ts

--- tests/src/app.test.ts
+++ tests/src/app.test.ts
@@ -1,3 +1,35 @@
+// process.argv = ["node", "app.ts", "-b", "10"];
+// import "../../src/app";
+import { ServerApp } from "../../src/presentation/server-app";
+
 describe("app.ts", () => {
-  test("", () => {});
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test("should call Server.run with values", async () => {
+    const serverRunMock = jest.fn();
+    ServerApp.run = serverRunMock;
+
+    process.argv = [
+      "node",
+      "app.ts",
+      "-b",
+      "10",
+      "-l",
+      "-s",
+      "-n",
+      "test-file",
+    ];
+
+    await import("../../src/app");
+
+    expect(serverRunMock).toHaveBeenCalledWith({
+      base: 10,
+      fileDestination: "outputs",
+      fileName: "test-file",
+      limit: 10,
+      showTable: true,
+    });
+  });
 });
